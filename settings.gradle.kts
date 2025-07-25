8ff0f45472f23056f06a5e10ec8c0c604aa5a590
rodcod24-droid <rodcod24@gmail.com>
2025-07-24 17:47:18 -0700

    Update settings.gradle.kts

--- settings.gradle.kts
+++ settings.gradle.kts
@@ -3,22 +3,17 @@ rootProject.name = "CloudstreamPlugins"
 // This file sets what projects are included.
 // All new projects should get automatically included unless specified in the "disabled" variable.
 
-val disabled = listOf<String>()
+//val disabled = listOf<String>()
 
-File(rootDir, ".").eachDir { dir ->
-    if (!disabled.contains(dir.name) && File(dir, "build.gradle.kts").exists()) {
-        include(dir.name)
-    }
-}
+//File(rootDir, ".").eachDir { dir ->
+//    if (!disabled.contains(dir.name) && File(dir, "build.gradle.kts").exists()) {
+//        include(dir.name)
+//    }
+//}
 
-fun File.eachDir(block: (File) -> Unit) {
-    listFiles()?.filter { it.isDirectory }?.forEach { block(it) }
-}
-rootProject.name = "CloudStreamPlugins"
-
-// Add each provider here
-include(":Cuevana")
-// include(":AnotherProvider")  // Add more providers like this
+//fun File.eachDir(block: (File) -> Unit) {
+//    listFiles()?.filter { it.isDirectory }?.forEach { block(it) }
+//}
 
 // To only include a single project, comment out the previous lines (except the first one), and include your plugin like so:
-// include("PluginName")
+include("Cuevana")
